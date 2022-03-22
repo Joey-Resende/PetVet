c9b0bb68ec71f9302cde49d658b3ae990fb3c627
Joey-Resende <givanildo.resende@gmail.com>
2022-03-22 10:07:27 -0300

    Create a home page

--- PetVet/core/urls.py
+++ PetVet/core/urls.py
@@ -2,5 +2,5 @@ from core import views
 from django.urls import path
 
 urlpatterns = [
-
+    path('', views.index, name='index'),
 ]
--- PetVet/core/views.py
+++ PetVet/core/views.py
@@ -1,3 +1,16 @@
+from core.models import SpeciesOfPet, Procedures, Doctor
 from django.shortcuts import render
+
+
+def index(request):
+    """View function for home page site."""
+    num_procedures = Procedures.objects.all().count()
+
+    context = {
+        'num_procedures': num_procedures,
+    }
+
+    return render(request, 'index.html', context=context)
+
 
 # Create your views here.
