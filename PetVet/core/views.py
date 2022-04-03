891b1395bab31c52040fe30562cb5dc721ddb788
Joey-Resende <givanildo.resende@gmail.com>
2022-04-03 20:12:55 -0300

    Refatorando o template do index

--- PetVet/core/views.py
+++ PetVet/core/views.py
@@ -6,7 +6,7 @@ from django.views.generic import TemplateView
 
 
 class IndexView(TemplateView):
-    template_name = 'index.html'
+    template_name = 'core/index.html'
     num_pets = Pets.objects.all().count()
     num_tutors = Tutor.objects.all().count()
     num_medical_care = MedicalCare.objects.all().count()
